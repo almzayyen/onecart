1fdfbf31581f1c69d07b94e437f6b727ef9baad2
Talal M <0talal.mash0@gmail.com>
2018-06-09 18:52:51 +0200

    added footer (post-commit)

---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+require ('./footer.css');
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <ul>
+                <li>
+                    Copyright Notice
+                </li>
+                <li>
+                    Terms of Use
+                </li>
+                <li>
+                    GitHub
+                </li>
+                
+            </ul>
+        </div>
+    );
+}
+
+export default Footer;
